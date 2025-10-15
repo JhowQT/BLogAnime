7f5c949aa9d95bf6969986138e4d444fa7092e04
JhonQT <jhon.q.torrez@gmail.com>
2025-10-15 11:07:47 -0300

    adicionando o usuario na pasta DTO e suas classes correspondente.

---
+++ src/main/java/br/com/fiap/BlogAnime/dto/UsuarioCreateDTO.java
@@ -0,0 +1,23 @@
+package br.com.fiap.BlogAnime.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioCreateDTO {
+
+    @NotBlank(message = "{user.name.notblank}")
+    private String nome;
+
+    @NotBlank(message = "{user.email.notblank}")
+    @Email(message = "{user.email.invalid}")
+    private String email;
+
+    @NotBlank(message = "{user.senha.notblank}")
+    private String senha;
+}
---
+++ src/main/java/br/com/fiap/BlogAnime/dto/UsuarioResponseDTO.java
@@ -0,0 +1,15 @@
+package br.com.fiap.BlogAnime.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioResponseDTO {
+
+    private Long idUsuario;
+    private String nome;
+    private String email;
+}
